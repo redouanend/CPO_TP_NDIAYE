8b95b3f4cdb59ee497056e0bea547b958f226e16
redouane ndiaye <oumar.ndiaye@epfedu.fr>
2023-09-26 16:00:09 +0200

    TP0_Exo1

---
+++ TP0_NDIAYE/Exo1.java
@@ -0,0 +1,24 @@
+/*Ndiaye Oumar Redouane 
+ *TP0
+ * Septembre 2023
+ */
+
+package com.mycompany.exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author redou
+ */
+public class Exo1 {
+
+    public static void main(String[] args) {
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Aurevoir");
+    }
+}
